f6afc65fe10068217d3637144f00769391f0f265
Sera Koukpaki <serapetrus@ymail.com>
2021-11-13 23:53:39 +0000

    seraHomewrk

---
+++ src/pet_shop.py
@@ -0,0 +1,23 @@
+# WRITE YOUR FUNCTIONS HERE
+def get_pet_shop_name(pet_shop):
+    return pet_shop["name"]
+
+def get_total_cash(pet_shop):
+    return pet_shop["admin"]["total_cash"]
+
+def get_total_cash(total_cash):
+    total_cash_result = total_cash(["admin"]["total cash"] +10)
+    return total_cash_result
+
+def get_total_cash(total_cash):
+    total_cash_result = total_cash(["admin"]["total_cash"] -10)
+    return total_cash_result
+
+def get_pets_sold(pets_sold):
+    return pets_sold["admin"]["pets_sold"]
+
+def increase_pets_sold(pets_sold,"2"):
+    return pets_sold["admin"]["pets_sold"] + 2
+
+def get_stock_count(len("pets")):
+    return len("pets")
