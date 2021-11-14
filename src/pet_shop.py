244e5a2ca02ef8270a93a1acb87529105312736f
Sera Koukpaki <serapetrus@ymail.com>
2021-11-14 15:24:05 +0000

    new

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -16,8 +16,46 @@ def get_total_cash(total_cash):
 def get_pets_sold(pets_sold):
     return pets_sold["admin"]["pets_sold"]
 
-def increase_pets_sold(pets_sold,"2"):
+def increase_pets_sold(pets_sold,):
     return pets_sold["admin"]["pets_sold"] + 2
 
-def get_stock_count(len("pets")):
-    return len("pets")
+def get_stock_count(stock_count):
+    stock_count = len(["pets"])
+    return stock_count
+
+def get_pets_by_breed_found(pets_by_breed):
+    pets_by_breed = ["pets"]["breed"][0][1]
+    return pets_by_breed
+
+def get_pets_by_breed(breed):
+    breed = self.cc_pet_shop["pets"]["0"]
+    return Dalmation
+
+def find_pet_by_name(pet_shop):
+    return pet_shop["pets"][3]["name"]
+
+def find_pet_name(pet_shop, name):
+    for name in pet_shop:
+        if pet_shop["name"] == ("Fred"):
+           return name
+    else:
+           return None
+
+def find_pet_name(pet_shop, name):
+    for name in pet_shop:
+        if pet_shop["name"] == ("Arthur"):
+            return name
+        else:
+            return None
+def get_stock_count(self_customers):
+    return self_customers[0]["cash"]
+
+def remove_customer_cash(self_customers):
+        return self_customers[0]["cash"] - 100
+
+def get_customer_pet_count(self_customers):
+    return self_customers[0]["pets"]
+
+def add_pet_to_customer(self_customers):
+    return self_customers[0]["pets"] + 1
+    
